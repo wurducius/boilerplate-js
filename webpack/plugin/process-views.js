2143f4835e19ff391df366840496db7192ece848
Jakub Eliáš <wurducius@gmail.com>
2025-07-07 23:28:04 +0200

    Refactor styling, add showcase image, extract fonts

--- webpack/plugin/process-views.js
+++ webpack/plugin/process-views.js
@@ -4,13 +4,7 @@ import { addAsset, CWD } from "./plugin-util.cjs"
 import { htmlTemplate } from "../compile/html-template.js"
 import { headData } from "../compile/head.js"
 
-const stylesPath = path.join(CWD, "resources", "styles")
-
-const stylePaths = [
-  path.join(stylesPath, "theme.css"),
-  path.join(stylesPath, "base.css"),
-  path.join(stylesPath, "simple.css"),
-]
+const stylesPath = path.join(CWD, "styles")
 
 const templatesPath = path.join(CWD, "templates")
 const projectPath = path.join(CWD, "src")
@@ -22,6 +16,9 @@ export const processViews = async (compiler, compilation) => {
   const views = (await fs.promises.readdir(templatesPath, { recursive: true }))
     .filter((filename) => filename.endsWith(".html"))
     .map((filename) => filename.substring(0, filename.lastIndexOf(".")) || filename)
+  const commonStylePaths = (await fs.promises.readdir(stylesPath, { recursive: true }))
+    .filter((filename) => filename.endsWith(".css"))
+    .map((filename) => path.join(stylesPath, filename))
   await Promise.all(
     views.map(async (view) => {
       const assetName = `${view}.html`
@@ -29,7 +26,9 @@ export const processViews = async (compiler, compilation) => {
         await fs.promises.readFile(getViewPath(view)).then((buffer) => {
           const content = buffer.toString()
           const customStylesheetPath = getStylesheetPath(view)
-          const styles = (fs.existsSync(customStylesheetPath) ? [...stylePaths, customStylesheetPath] : stylePaths)
+          const styles = (
+            fs.existsSync(customStylesheetPath) ? [...commonStylePaths, customStylesheetPath] : commonStylePaths
+          )
             .map((stylePath) => fs.readFileSync(stylePath).toString())
             .join(" ")
           return htmlTemplate({ ...headData, styles, content })
